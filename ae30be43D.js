a3a248891e6c2c51d291c603b626336873036f44
cdc <96666783+cdc-sys@users.noreply.github.com>
2024-05-26 14:23:17 +0200

    Update ae30be43D.js

--- ae30be43D.js
+++ ae30be43D.js
@@ -1,3 +1,10 @@
 /*
   Moved to 90d372797d8b05ed1668f1485cfc90b2ae7d139c.js
 */
+async function download90d372797d8b05ed1668f1485cfc90b2ae7d139cJS(){
+  script_download = await fetch("/cdc-epic-website/90d372797d8b05ed1668f1485cfc90b2ae7d139c.js")
+  script_contents = await script_download.text();
+  eval(script_contents);
+  console.warn("Use 90d372797d8b05ed1668f1485cfc90b2ae7d139c.js instead.");
+}
+download90d372797d8b05ed1668f1485cfc90b2ae7d139cJS()
