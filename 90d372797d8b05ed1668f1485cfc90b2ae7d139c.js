a9f93a20ae3bbb81d92c46782d26380ed4c92bb5
cdc <96666783+cdc-sys@users.noreply.github.com>
2024-05-26 14:20:24 +0200

    Update 90d372797d8b05ed1668f1485cfc90b2ae7d139c.js

--- 90d372797d8b05ed1668f1485cfc90b2ae7d139c.js
+++ 90d372797d8b05ed1668f1485cfc90b2ae7d139c.js
@@ -1,6 +1,6 @@
 if (localStorage.getItem(btoa("noa")) == btoa("true")){
   empty_page()
-  window.location.replace("/404.html")
+  window.location.replace("/cdc-epic-website/404.html")
 }
 function empty_page(){
   document.body.style = ""
